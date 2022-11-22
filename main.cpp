9380480ce12e70aae02b1c9baaa4d8da28c2599d
claudeus123 <claudio.pizarro@alumnos.ucn.cl>
2022-11-22 20:21:32 -0300

    cambio

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "Domain/Headers/makeRegisters.h"
 #include "Domain/Headers/system.h"
+#include "Domain/Headers/linkedList.h"
+#include "Domain/Headers/region.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -8,23 +10,41 @@
 using namespace std;
 
 int main() {
+	int rf;
+	cout << "1) ArrayList + BubbleSort " << endl;
+	cout << "2) ArrayList + HeapSort " << endl;
+	cout << "3) LinkedList + BubbleSort " << endl;
+	cout << "4) LinkedList + HeapSort " << endl;
+	cout << "Ingrese un numero: " <<endl;
+	cin >> rf;
         int regionsSize = 16;
-		string pathToFile = "el.csv";
-        makeRegisters system = makeRegisters();
-        system.readFile(pathToFile);
+	string pathToFile = "/home/data/Microdato_Censo2017-Personas.csv";
+        
+	if (rf == 1){
+		makeRegisters rf1 = makeRegisters();
+        	rf1.readFile(pathToFile);
 		string reference[] = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"}; 
-		system.sort(reference, system.getRegions(), system.getRegisters(), 16);
-        system.printFormat(system.getRegions(),system.getRegisters(),regionsSize);
-
-	
-		system.heapSort(system.getRegions(),system.getRegisters(),16);
-		system.printFormat(system.getRegions(), system.getRegisters(),16);
-
+		rf1.sort(reference, rf1.getRegions(), rf1.getRegisters(), 16);
+	        rf1.printFormat(rf1.getRegions(),rf1.getRegisters(),regionsSize);	
+	} else if (rf == 2){
+		makeRegisters rf2 = makeRegisters();
+		rf2.readFile(pathToFile);
+		rf2.heapSort(rf2.getRegions(),rf2.getRegisters(),16);
+		rf2.printFormat(rf2.getRegions(), rf2.getRegisters(),16);
+	} else if (rf == 3){
 		LinkedList ll;
-		System system2 = System(ll);
-		system2.readFile(pathToFile);
-		system2.print();
-
-		system2.printHeapSort();
-		system2.deleteList();
+		System rf3 = System(ll);
+		rf3.readFile(pathToFile);
+		rf3.print();
+		rf3.deleteList();
+	} else if (rf == 4){
+		LinkedList ll;
+		System rf4 = System(ll);
+		rf4.readFile(pathToFile);
+		rf4.printHeapSort();
+		rf4.deleteList();
+	} else {
+		cout << "Codigo no valido" << endl;
+	}
+	
 }
